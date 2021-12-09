7a8eb8c6815478956886a29501e765c575c36f8c
Mihail Sudenoc <msudenoc@pentalog.com>
2021-12-09 18:53:26 +0200

    Class component style tryout

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { Component } from 'react';
 
 // image
 import searchIcon from '../../images/search-icon.svg';
@@ -6,35 +6,64 @@ import searchIcon from '../../images/search-icon.svg';
 // styles
 import { Wrapper, Content } from './SearchBar.styles';
 
-const SearchBar = ({ setSearchTerm }) => {
-  const [state, setState] = useState('');
-  const initial = useRef(true);
+export class SearchBar extends Component {
+  state = { value: '' };
+  timeout = null;
 
-  useEffect(() => {
-    if (initial.current) {
-      initial.current = false;
+  componentDidUpdate(_prevProps, prevState) {
+    if (this.state.value !== prevState.value) {
+      const { setSearchTerm } = this.props;
+
+      clearTimeout(this.timeot);
+
+      this.timeout = setTimeout(() => {
+        const { value } = this.state;
+        setSearchTerm(value);
+      }, 500);
     }
+  }
 
-    const timer = setTimeout(() => {
-      setSearchTerm(state);
-    }, 500);
+  render() {
+    return (
+      <Wrapper>
+        <Content>
+          <img src={searchIcon} alt='search-icon' />
+          <input
+            type='text'
+            placeholder='SearchMovie'
+            onChange={(e) => this.setState({ value: e.currentTarget.value })}
+            value={this.state.value}
+          />
+        </Content>
+      </Wrapper>
+    );
+  }
+}
 
-    return () => clearTimeout(timer);
-  }, [setSearchTerm, state]);
+// const SearchBarFunc = ({ setSearchTerm }) => {
+//   const [state, setState] = useState('');
+//   const initial = useRef(true);
 
-  return (
-    <Wrapper>
-      <Content>
-        <img src={searchIcon} alt='search-icon' />
-        <input
-          type='text'
-          placeholder='SearchMovie'
-          onChange={(e) => setState(e.currentTarget.value)}
-          value={state}
-        />
-      </Content>
-    </Wrapper>
-  );
-};
+//   useEffect(() => {
+//     if (initial.current) {
+//       initial.current = false;
+//     }
+
+//     const timer = setTimeout(() => {
+//       setSearchTerm(state);
+//     }, 500);
+
+//     return () => clearTimeout(timer);
+//   }, [setSearchTerm, state]);
+
+//   return (
+//     <Wrapper>
+//       <Content>
+//         <img src={searchIcon} alt='search-icon' />
+//         <input type='text' placeholder='SearchMovie' onChange={(e) => setState(e.currentTarget.value)} value={state} />
+//       </Content>
+//     </Wrapper>
+//   );
+// };
 
 export default SearchBar;
